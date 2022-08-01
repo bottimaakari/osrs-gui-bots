c2b83c0ee60c5c98ab661ae4fe69661e61c5dfd0
bottimaakari <bottimaakari@github>
2022-08-01 13:48:03 +0300

    add humidify to build script.

--- build_all.bat
+++ build_all.bat
@@ -9,6 +9,8 @@ START /WAIT "HIGH ALCHEMY" "%APPDATA%\Python\Python39\Scripts\pyinstaller.exe" -
 
 START /WAIT "NIGHTMARE" "%APPDATA%\Python\Python39\Scripts\pyinstaller.exe" --onefile .\nightmare\nightmare_clicker.py
 
+START /WAIT "HUMIDIFY" "%APPDATA%\Python\Python39\Scripts\pyinstaller.exe" --onefile .\humidify\humidify_clicker.py
+
 @REM COPY DIST BINARIES WITH ASSETS TO CORRESPONDING DIRS
 
 MKDIR .\dist\high_alchemy\
@@ -20,5 +22,9 @@ MKDIR .\dist\nightmare\
 XCOPY /Y .\dist\nightmare_clicker.exe .\dist\nightmare\
 XCOPY /Y .\nightmare\items-example.txt .\dist\nightmare\
 XCOPY /Y .\nightmare\settings-example.txt .\dist\nightmare\
+
+MKDIR .\dist\humidify\
+XCOPY /Y .\dist\humidify_clicker.exe .\dist\humidify\
+XCOPY /Y .\humidify\settings-example.txt .\dist\humidify\
 
 echo FINISHED BUILD
