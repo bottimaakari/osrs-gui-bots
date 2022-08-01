55cddd1ff3fa9ef47ae228c913daf206b4165982
bottimaakari <bottimaakari@github>
2022-08-01 13:48:22 +0300

    feat: implement better rng using 'quantumrandom' lib as random data source. update requirements.txt.

--- clicker_common.py
+++ clicker_common.py
@@ -1,6 +1,8 @@
 import time
+from typing import List
 
 import pyautogui
+import quantumrandom
 
 import globvals
 
@@ -17,6 +19,88 @@ class Timer:
     # Returns elapsed time floored to ms
     def elapsed(self) -> int:
         return int((time.perf_counter() - self._stamp) * 1000)
+
+
+class Random:
+    CHUNK_SIZE: int = 1024  # API Limit: 1024
+    VALUE_SIZE: int = 16  # API: int16 -> 16
+    DATA_COUNT: int = 30  # Values in total: DATA_COUNT * DATA_SIZE
+
+    def __init__(self, count: int = None, size: int = None):
+        self._data_count = count if count is not None else self.DATA_COUNT
+        self._data_size = size if size is not None else self.CHUNK_SIZE
+        self._data: List[List[int]] = []
+        self._len_data: int = 0
+        self._list_it: int = 0
+        self._value_it: int = -1
+        self._limit: int = 2 ** self.VALUE_SIZE
+        self._call_count = 0
+        self.init()
+
+    def __get_value(self) -> int:
+        self._call_count += 1
+        if self._value_it >= self._data_size - 1:
+            self._list_it += 1
+            self._value_it = -1
+        self._value_it += 1
+        return self._data[self._list_it][self._value_it]
+
+    def init(self) -> None:
+        """
+        (Re)initializes the rng with fresh values.
+        Can be called to ensure enough random values are buffered.
+        CAUTION! Most expensive method, use only when necessary
+        :return:
+        """
+        print("Initializing RNG..")
+
+        self._data.clear()
+
+        for i in range(self._data_count):
+            self._data.append(quantumrandom.get_data('uint16', self._data_size))
+
+        # Define len data as constant for later use
+        # Reset list pointers
+        self._len_data = len(self._data)
+        self._list_it = 0
+        self._value_it = -1
+
+        # Sanity check -> does the generated data match anyclose what was requested
+        if self._len_data < self._data_count or len(self._data[self._len_data - 1]) < self._data_size:
+            raise Exception("Failed to initialize RNG: Data count mismatch!")
+
+        print("Initializing RNG done.")
+
+    def random(self) -> float:
+        """
+        Returns a random float value in range [0, 1)
+        Uniformly distributed between [0, BIT_COUNT - 1]
+        :return:
+        """
+        # Since we are dealing with int16 (16 bits integer)
+        # To get a value between 0..1 we need to break down the random value into 2^16 pieces
+        # Then return the percentual value of the current number (to distribute value uniformly)
+        return self.__get_value() / self._limit
+
+    def randint(self, lo: int, hi: int) -> int:
+        """
+        Returns a random integer between a given range, [lo, hi]
+        Uniformly distributed
+        :param lo:
+        :param hi:
+        :return:
+        """
+        rng = hi - lo
+
+        # First check that if the range is valid
+        if rng < 0:
+            raise ValueError("Max cannot be less than Min")
+
+        # Limit the range to the maximum precision of 2^16
+        if rng > self._limit:
+            raise ValueError(f"Distance between min and max cannot be over {self._limit}")
+
+        return round(self.random() * rng) + lo
 
 
 def read_settings(filename: str):
@@ -41,7 +125,7 @@ def read_settings(filename: str):
 
 def read_inventory(filename: str):
     try:
-        file = open(filename, "r")
+        file = open(filename, "r", encoding="utf-8")
     except IOError as exc:
         print(f"ERROR: Failed to read item file ({filename}). Ensure it exists in the current directory.")
         raise exc
@@ -59,10 +143,8 @@ def read_inventory(filename: str):
 
 
 def save_inventory(filename: str, items: list):
-    file = None
-
     try:
-        file = open(filename, "r")
+        file = open(filename, "r", encoding="utf-8")
     except IOError as ex:
         print(
             f"ERROR: Failed to write into item file ({filename}). Ensure this executable and the current user has "
@@ -75,6 +157,11 @@ def save_inventory(filename: str, items: list):
         file.write("\r\n")  # TODO CRLF on win, LF on *nix
 
     file.close()
+
+
+def init_rng():
+    rng = Random()
+    return rng
 
 
 def rand_sleep(rng, minms: int, maxms: int, debug: bool = True):
